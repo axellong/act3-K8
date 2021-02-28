6a6eba35171766ccf9f937d64cda5ddd65c86c1b
axellong <193250@ids.upchiapas.edu.mx>
2021-02-28 14:35:31 -0600

    solution reach in mongo

---
+++ query.sql
@@ -0,0 +1,3 @@
+db.createCollection("Test")
+
+db.Test.insert({TestName:'Test',TestNumericInteger:1234567,TestDecimal:123567.220,TestTimestamp:Date(),TestDate:Date(),TestFloat:12345678.22,TestInteger:345678,TestComment:'Prueba test'})     
